557477ec2372f01f1f364eb303e83d75e9c3d4dd
HervalMata <hervalmataamparo@gmail.com>
2020-11-28 19:15:23 -0300

    Router and Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
+import {Route, Switch} from "react-router-dom";
 
-const App = () => (
-    <div>
-        <p>
-          react app.
-        </p>
-    </div>
-);
+const App = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/register" component={Register}/>
+        </Switch>
+    );
+};
 
 export default App;
---
+++ client/src/pages/auth/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => (
+    <div>
+        <p>
+            Cadastro
+        </p>
+    </div>
+);
+
+export default Register;
