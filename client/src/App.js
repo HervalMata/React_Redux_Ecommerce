774af9a76f1ea1493172212348d6e2dbb6f558e1
HervalMata <hervalmataamparo@gmail.com>
2020-11-29 19:08:04 -0300

    User Logout

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,28 @@ import Header from "./components/nav/Header";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import RegisterComplete from "./pages/auth/RegisterComplete";
+import { useDispatch } from "react-redux";
+import {auth} from "firebase";
 
 const App = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(async (user) => {
+            if (user) {
+                const idTokenResult = await user.getIdTokenResult();
+                console.log("user", user);
+                dispatch({
+                    type: "LOGGED_IN_USER",
+                    payload: {
+                        email: user.email,
+                        token: idTokenResult.token,
+                    },
+                });
+            }
+        });
+        return () => unsubscribe();
+    }, []);
+
     return (
         <>
             <Header />
