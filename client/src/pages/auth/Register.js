e2889424eec480af5658e5a9d5ccbcee0c8a0f03
HervalMata <hervalmataamparo@gmail.com>
2020-11-29 15:54:34 -0300

    Email Link To Signin

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {auth} from "../../firebase";
+import {toast, ToastContainer} from "react-toastify";
 
-const Register = () => (
-    <div>
-        <p>
-            Cadastro
-        </p>
-    </div>
-);
+const Register = () => {
 
+    const [email, setEmail] = useState("");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const config = {
+            url: "http://localhost:3000/register/complete",
+            handleCodeInApp: true,
+        };
+
+        await auth.sendSignInLinkToEmail(email, config);
+        toast.success(`Email foi enviado para ${email}. Click no link para completar seu cadastro.`);
+        window.localStorage.setItem("emailForRegistration", email);
+        setEmail("");
+    };
+
+    const registerForm = () => (
+        <form onSubmit={handleSubmit}>
+            <input type="email" className="form-control" value={email}
+            onChange={(e) => setEmail(e.target.value)} autoFocus/>
+            <button type="submit" className="btn btn-raised">
+                Cadastrar
+            </button>
+        </form>
+    );
+
+    return (
+        <div className="container p-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h4>Cadastrar</h4>
+                    <ToastContainer />
+                    {registerForm()}
+                </div>
+            </div>
+        </div>
+    );
+};
 export default Register;
