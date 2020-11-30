4800e3ee9566e366dfec1aa11b25aac74f616f8a
HervalMata <hervalmataamparo@gmail.com>
2020-11-30 15:34:45 -0300

    Routes Autoloading

--- server/server.js
+++ server/server.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { readdirSync } = require("fs");
 require("dotenv").config();
-
-const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -19,7 +18,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
 
-app.use("/api", authRoutes);
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 const port = process.env.PORT || 8000;
 
