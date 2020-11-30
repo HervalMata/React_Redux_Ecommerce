6b4698cdfad88d58d658740bb63781c688ac91cd
HervalMata <hervalmataamparo@gmail.com>
2020-11-30 15:12:30 -0300

    Server Setup

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+mongoose.connect(process.env.BATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+}).then(() => console.log("DB CONNECTED")).catch((err) => console.log("DB CONNECTED ERR", err));
+
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
+
+app.get("/api", (req, res) => {
+    res.json({
+        data: "hey you hit node API",
+    });
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
