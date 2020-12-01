b91abf6fff05e7ee26cb714d7b758cb70017bfea
HervalMata <hervalmataamparo@gmail.com>
2020-11-30 22:40:04 -0300

    Middlewares In NodeJS

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -6,3 +6,5 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://react-redux-ecommerce-58101.firebaseio.com"
 });
+
+module.exports = admin;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { authCheck } = require("../middlewares/auth")
 const { createOrUpdateUser } = require("../controllers/auth");
 
-router.get("/create-or-update-user", createOrUpdateUser);
+router.get("/create-or-update-user", authCheck, createOrUpdateUser);
 
 module.exports = router;
