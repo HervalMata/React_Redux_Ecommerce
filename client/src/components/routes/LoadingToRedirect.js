a2948c651e82c03bd79f92f50f6159c603ee64d4
HervalMata <hervalmataamparo@gmail.com>
2020-12-01 18:03:35 -0300

    Redirect Countdown

---
+++ client/src/components/routes/LoadingToRedirect.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from "react";
+import { useHistory } from 'react-router-dom';
+
+const LoadingToRedirect  = () => {
+    const [count, setCount] = useState(5);
+    let history = useHistory();
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount((currentCount) => --currentCount);
+        }, 1000);
+        count === 0 && history.push("/");
+        return () => clearInterval(interval);
+    }, [count]);
+    return (
+        <div className="container p-5 text-center">
+            <p>Redirecionando você em {count} segundos</p>
+        </div>
+    );
+};
+
+export default LoadingToRedirect;
--- client/src/components/routes/UserRoutes.js
+++ client/src/components/routes/UserRoutes.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {Route} from "react-router-dom";
 import {useSelector} from "react-redux";
+import LoadingToRedirect from "./LoadingToRedirect";
 
 const UserRoute = ({ children, ...rest }) => {
     const { user } = useSelector((state) => ({ ...state }));
     return user && user.token ? (
         <Route { ...rest } render={() => children} />
     ) : (
-        <h1 className="text-danger">Carregando...</h1>
+        <LoadingToRedirect />
     );
 };
 
