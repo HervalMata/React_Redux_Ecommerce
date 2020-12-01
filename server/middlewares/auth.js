3a4231d8d243f869d0c894b77e0f6b7f27fe7860
HervalMata <hervalmataamparo@gmail.com>
2020-11-30 23:51:55 -0300

    Create Or Update User

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,14 @@
 const admin = require("../firebase");
 
-exports.authCheck = (req, res, next) => {
-    console.log(req.headers);
-    next();
+exports.authCheck = async (req, res, next) => {
+    //console.log(req.headers);
+    try {
+        const firebaseUser = await admin.auth().verifyIdToken(req.headers.authToken);
+        req.user = firebaseUser;
+        next();
+    } catch (err) {
+        res.status(401).json({
+            err: "Invalid or expired token",
+        });
+    }
 };
