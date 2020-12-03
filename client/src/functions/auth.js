2abde4a7996a6a099f426cfb59121f235925b966
HervalMata <hervalmataamparo@gmail.com>
2020-12-03 14:08:25 -0300

    Category CRUD Requests

--- client/src/functions/auth.js
+++ client/src/functions/auth.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 
-export const createOrUpdateUser = async (authToken) => {
+export const createOrUpdateUser = async (authtoken) => {
     return await axios.post(
         `${process.env.REACT_APP_API}/create-or-update-user`,
-        {}, { headers: { authToken, }, }
+        {}, { headers: { authtoken, }, }
     );
 };
 
-export const currentUser = async (authToken) => {
+export const currentUser = async (authtoken) => {
     return await axios.post(
         `${process.env.REACT_APP_API}/current-user`,
-        {}, { headers: { authToken, }, }
+        {}, { headers: { authtoken, }, }
     );
 };
 
-export const currentAdmin = async (authToken) => {
+export const currentAdmin = async (authtoken) => {
     return await axios.post(
         `${process.env.REACT_APP_API}/current-admin`,
-        {}, { headers: { authToken, }, }
+        {}, { headers: { authtoken, }, }
     );
 };
