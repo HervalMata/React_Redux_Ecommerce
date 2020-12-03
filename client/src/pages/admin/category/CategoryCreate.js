bf8f834de05c629311c6e9e38edb9ab4107e3573
HervalMata <hervalmataamparo@gmail.com>
2020-12-03 19:56:19 -0300

    Category Create And List

--- client/src/pages/admin/category/CategoryCreate.js
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import AdminNav from "../../../components/nav/AdminNav";
+import {useSelector} from "react-redux";
+import {createCategory, getCategories} from "../../../functions/category";
+import {toast} from "react-toastify";
 
 const CategoryCreate = () => {
+    const { user } = useSelector((state) => ({ ...state }));
+    const [name, setName] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        loadCategories();
+    });
+
+    const loadCategories = () =>
+        getCategories().then((c) => setCategories(c.data));
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        createCategory({ name }, user.token)
+            .then((res) => {
+                setLoading(false);
+                setName("");
+                toast.success(`"${res.data.name}" está criado`);
+            })
+            .catch((err) => {
+                console.log(err);
+                setLoading(false);
+                if (err.response.status === 400) toast.error(err.response.data);
+            });
+    };
+
+    const categoryForm = () => (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Nome</label>
+                <input
+                    type="text" className="form-control"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name} autoFocus required
+                />
+                <br/>
+                <button className="btn btn-outline-primary">Salvar</button>
+            </div>
+        </form>
+    );
+
     return (
         <div className="container-fluid">
             <div className="row">
@@ -9,7 +55,14 @@ const CategoryCreate = () => {
                     <AdminNav />
                 </div>
                 <div className="col">
-                    Cadastrar Categoria
+                    {loading ? (
+                        <h4 className="text-danger">Carregando...</h4>
+                    ) : (
+                        <h4>Cadastrar Categoria</h4>
+                    )}
+                    {categoryForm()}
+                    <hr />
+                    {JSON.stringify(categories)}
                 </div>
             </div>
         </div>
