af1cf8b96916d8128fb8956ecb6e1a3d1d298d85
HervalMata <hervalmataamparo@gmail.com>
2020-12-04 23:22:21 -0300

    Code Refactor Product

---
+++ client/src/components/forms/ProductCreateForm.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+const ProductCreateForm = ({ handleSubmit, handleChange, values }) => {
+    const {
+        title, description, price, categories, category,
+        shipping, quantity, images, colors, color
+    } = values;
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Titulo</label>
+                <input
+                    type="text"  name="title"
+                    className="form-control"
+                    value={title} onChange={handleChange}
+                />
+            </div>
+            <div className="form-group">
+                <label>Descrição</label>
+                <input
+                    type="text"  name="description"
+                    className="form-control"
+                    value={description} onChange={handleChange}
+                />
+            </div>
+            <div className="form-group">
+                <label>Preço</label>
+                <input
+                    type="number"  name="price"
+                    className="form-control"
+                    value={price} onChange={handleChange}
+                />
+            </div>
+            <div className="form-group">
+                <label>Entrega</label>
+                <select
+                    name="shipping"
+                    className="form-control"
+                    value={title} onChange={handleChange}
+                >
+                    <option>Por favor selecione</option>
+                    <option value="No">Não</option>
+                    <option value="Yes">Sim</option>
+                </select>
+            </div>
+            <div className="form-group">
+                <label>Quantidade</label>
+                <input
+                    type="number"  name="quantity"
+                    className="form-control"
+                    value={quantity} onChange={handleChange}
+                />
+            </div>
+            <div className="form-group">
+                <label>Cor</label>
+                <select
+                    name="color"
+                    className="form-control"
+                    value={title} onChange={handleChange}
+                >
+                    <option>Por favor selecione</option>
+                    {colors.map((c) => (
+                        <option key={c} value={c}>{c}</option>
+                    ))}
+                </select>
+            </div>
+            <button className="btn btn-outline-info">Salvar</button>
+        </form>
+    );
+};
+
+export default ProductCreateForm;
