ed4c0958954d2abf8ef31981d4e8f3a72d7297f6
HervalMata <hervalmataamparo@gmail.com>
2020-12-04 23:02:23 -0300

    Product Create Error Message

--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -35,7 +35,8 @@ const ProductCreate = () => {
             window.location.reload();
         }).catch((err) => {
             console.log(err);
-            if (err.response.status === 400) toast.error(err.response.data);
+            //if (err.response.status === 400) toast.error(err.response.data);
+            toast.error(err.response.data.err);
         });
     };
 
