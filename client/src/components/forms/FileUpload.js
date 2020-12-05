c0f6aab70fc5c29202f0d25448c044aa26bb92d3
HervalMata <hervalmataamparo@gmail.com>
2020-12-05 02:47:39 -0300

    Product Create With Multiple Image

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {useSelector} from "react-redux";
 import Resizer from "react-image-file-resizer";
 import axios from 'axios';
-import { Avatar } from 'antd';
+import {Avatar, Badge} from 'antd';
 
 const FileUpload = ({ values, setValues, setLoading }) => {
     const { user } = useSelector((state) => ({ ...state }));
@@ -36,16 +36,43 @@ const FileUpload = ({ values, setValues, setLoading }) => {
         }
     };
 
+    const handleImageRemove = (public_id) => {
+        setLoading(true);
+        axios.post(`${process.env.REACT_APP_API}/removeimage`,
+            { public_id },
+            {
+                headers: {
+                    authtoken: user ? user.token : "",
+                },
+            }).then((res) => {
+                setLoading(false);
+                const { images } = values;
+                let filteredImages = images.filter((item) => {
+                    return item.public_id !== public_id;
+                });
+                setValues({ ...values, images: filteredImages });
+            }).catch((err) => {
+                setLoading(false);
+                console.log(err);
+            });
+    };
+
     return (
         <>
             <div className="row">
                 {values.images && values.images.map((image) => (
+                    <Badge
+                        count="X" key={image.public_id}
+                        onClick={() => handleImageRemove(image.public_id)}
+                        style={{ cursor: "pointer" }}
+                    >
                     <Avatar
                         key={image.public_id}
                         src={image.url}
                         size={100}
                         className="m-3"
                     />
+                    </Badge>
                 ))}
             </div>
             <div className="row">
