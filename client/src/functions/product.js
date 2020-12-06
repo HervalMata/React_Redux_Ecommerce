2d57af4c102f31c7ff87a65f7da66917f3285bd0
HervalMata <hervalmataamparo@gmail.com>
2020-12-06 00:31:09 -0300

    Fetch All Products In Admin Dashboard

--- client/src/functions/product.js
+++ client/src/functions/product.js
@@ -6,3 +6,13 @@ export const createProduct = async (product, authtoken) =>
             authtoken,
         },
     });
+
+export const createProduct = async (product, authtoken) =>
+    await axios.post(`${process.env.REACT_APP_API}/product`, product, {
+        headers: {
+            authtoken,
+        },
+    });
+
+export const getProductsByCount = async (count) =>
+    await axios.get(`${process.env.REACT_APP_API}/products/${count}`);
--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import AdminNav from "../../components/nav/AdminNav";
+import {getProductsByCount} from "../../functions/product";
 
 const AdminDashboard = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        loadAllProducts();
+    }, []);
+
+    const loadAllProducts = () => {
+        setLoading(true);
+        getProductsByCount(100)
+            .then((res) => {
+                setProducts(res.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                setLoading(false);
+                console.log(err);
+            });
+    };
+
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-md-2">
                     <AdminNav />
                 </div>
+                {loading ? (
+                    <h4 className="text-danger">Carregando...</h4>
+                ) : (
+                    <h4>Todos os produtos</h4>
+                )}
                 <div className="col">
-                    Admin Dashboard
+                    {JSON.stringify(products)}
                 </div>
             </div>
         </div>
