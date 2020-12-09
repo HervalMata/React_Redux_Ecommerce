b8a762a16f45087f78ea9c4de26c56e4a52eee7f
HervalMata <hervalmataamparo@gmail.com>
2020-12-09 16:37:15 -0300

    List Products With Sort Order Limit

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -64,3 +64,17 @@ exports.update = async (req, res) => {
         });
     }
 };
+
+exports.list = async (req, res) => {
+    try {
+        const { sort, order, limit } = req.body;
+        const products = await Product.find({})
+            .populate("category")
+            .sort([[sort, order]])
+            .limit(limit)
+            .exec();
+        res.json(products);
+    } catch (err) {
+        console.log(err);
+    }
+};
--- server/routes/product.js
+++ server/routes/product.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const { authCheck, adminCheck } = require("../middlewares/auth");
 
-const { create, listAll, remove, read, update } = require("../controllers/product");
+const { create, listAll, remove, read, update, list } = require("../controllers/product");
 
 router.post("/product", authCheck, adminCheck, create);
 router.get("/products/:count", listAll);
 router.delete("/product/:slug", authCheck, adminCheck, remove);
 router.get("/product/:slug", read);
 router.put("/product/:slug", authCheck, adminCheck, update);
+router.get("/products", list);
 
 module.exports = router;
