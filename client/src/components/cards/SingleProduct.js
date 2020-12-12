00ba0d35cc6e46981f2bc82bf961de5682617293
HervalMata <hervalmataamparo@gmail.com>
2020-12-11 21:20:08 -0300

    Show Average Rating

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -7,6 +7,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import ProductListItems from "./ProductListItems";
 import StarRating from "react-star-ratings";
 import RatingModal from "../modal/RatingModal";
+import {showAverage} from "../../functions/rating";
 
 const { TabPane } = Tabs;
 
@@ -31,6 +32,8 @@ const SingleProduct = ({ product, onStarClick, star }) => {
             </div>
             <div className="col-md-5">
                 <h1 className="bg-info p-3">{title}</h1>
+                {product && product.ratings && product.ratings.length > 0
+                 ? showAverage(product) : "Nenhuma avaliação ainda"}
                 <Card actions={[
                     <>
                         <ShoppingCartOutlined className="text-success" />
