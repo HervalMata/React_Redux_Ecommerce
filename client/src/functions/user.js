d8852396db3caefc77f735b56c33aa6783a9d77e
HervalMata <hervalmataamparo@gmail.com>
2020-12-15 18:23:05 -0300

    User Orders Purchase History Page

--- client/src/functions/user.js
+++ client/src/functions/user.js
@@ -63,3 +63,13 @@ export const createOrder = async (stripeResponse, authtoken) =>
             },
         }
     );
+
+export const getUserOrders = async (authtoken) =>
+    await axios.get(
+        `${process.env.REACT_APP_API}/user/orders`,
+        {
+            headers: {
+                authtoken,
+            },
+        }
+    );
--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import UserNav from "../../components/nav/UserNav";
+import {useSelector} from "react-redux";
+import {getUserOrders} from "../../functions/user";
 
-const History = () => (
-    <div className="container-fluid">
-        <div className="row">
-            <div className="col-md-2">
-                <UserNav />
-            </div>
-            <div className="col">
-                User History Page
+const History = () => {
+    const [orders, setOrders] = useState([]);
+    const { user } = useSelector((state) => ({ ...state }));
+
+    useEffect(() => {
+        loadUserOrders();
+    }, []);
+
+    const loadUserOrders = () =>
+        getUserOrders(user.token).then((res) => {
+            console.log(JSON.stringify(res.data, null, 4));
+            setOrders(res.data);
+        });
+
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-2">
+                    <UserNav/>
+                </div>
+                <div className="col">
+                    User History Page
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default History;
