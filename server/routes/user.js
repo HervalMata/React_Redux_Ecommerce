4a627d0451da93f171036e5704be6f07182076ff
HervalMata <hervalmataamparo@gmail.com>
2020-12-15 17:37:15 -0300

    User Orders Backend

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const { authCheck } = require("../middlewares/auth");
 
-const { userCart, getUserCart, emptyUserCart, saveAddress, applyCouponToUserCart, createOrder } = require("../controllers/user");
+const { userCart, getUserCart, emptyUserCart, saveAddress, applyCouponToUserCart, createOrder, orders } = require("../controllers/user");
 
 router.post("/user/cart", authCheck, userCart);
 router.get("/user/cart", authCheck, getUserCart);
@@ -12,5 +12,6 @@ router.delete("/user/cart", authCheck, emptyUserCart);
 router.post("/user/address", authCheck, saveAddress);
 router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
 router.post("/user/order", authCheck, createOrder);
+router.get("/user/orders", authCheck, orders);
 
 module.exports = router;
