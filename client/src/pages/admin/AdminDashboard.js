94ee6d7fe2d146c0db9a70b021b5ebc7005655f6
HervalMata <hervalmataamparo@gmail.com>
2020-12-16 00:13:33 -0300

    Load All Orders On Admin Dashboard

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import AdminNav from "../../components/nav/AdminNav";
+import {changeStatus, getOrders} from "../../functions/admin";
+import {toast} from "react-toastify";
+import {useSelector} from "react-redux";
 
 const AdminDashboard = () => {
+
+    const [orders, setOrders] = useState([]);
+    const { user } = useSelector((state) => ({ ...state }));
+
+    useEffect(() => {
+        loadOrders();
+    }, []);
+
+
+    const loadOrders = () =>
+        getOrders(user.token).then((res) => {
+            console.log(JSON.stringify(res.data, null, 4));
+            setOrders(res.data);
+        });
+
+    const handleStatusChange = (orderId, orderStatus) => {
+        changeStatus(orderId, orderStatus).then((res) => {
+            toast.success("Status Atualizado.");
+            loadOrders();
+        })
+    }
+
     return (
         <div className="container-fluid">
             <div className="row">
@@ -10,6 +35,7 @@ const AdminDashboard = () => {
                 </div>
                 <div className="col">
                     <h4>Admin Dashboard</h4>
+                    {JSON.stringify(orders)}
                 </div>
             </div>
         </div>
